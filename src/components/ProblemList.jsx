d00a286fa70afa2ec99c3bc92f351b77c3a67aae
OnjoujiToki <z@moon.moe>
2023-11-27 23:00:56 -0500

    update ProblemList for potential submission fetch

--- src/components/ProblemList.jsx
+++ src/components/ProblemList.jsx
@@ -12,16 +12,42 @@ import {
 } from 'firebase/firestore';
 import { db, auth } from '../config/firebase'; // Adjust the import path
 import './componentsCSS/Problem.css';
+
 function ProblemList({ problems, showTags, listName, onDelete, planId }) {
   const [currentPage, setCurrentPage] = useState(1);
   const problemsPerPage = 50;
-
   const [userHandles, setUserHandles] = useState([]);
   const [planDetails, setPlanDetails] = useState(null);
-  const fetchSubmissionsForHandle = async (handle) => {
-    // Placeholder function. Replace with actual logic to fetch submissions.
-    return []; // Return an array of submission objects.
-  };
+  const indexOfLastProblem = currentPage * problemsPerPage;
+  const indexOfFirstProblem = indexOfLastProblem - problemsPerPage;
+  const [localProblems, setLocalProblems] = useState(problems); // Local state to manage problems
+
+  const currentProblems = problems.slice(
+    indexOfFirstProblem,
+    indexOfLastProblem
+  );
+
+  useEffect(() => {
+    const fetchUserHandles = async () => {
+      if (auth.currentUser) {
+        const userDocRef = doc(db, 'users', auth.currentUser.uid);
+        try {
+          const userDocSnap = await getDoc(userDocRef);
+          if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+            setUserHandles(userData.handles || []);
+          } else {
+            console.log('User document not found');
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+
+    fetchUserHandles();
+  }, []);
+
   useEffect(() => {
     const fetchPlanDetails = async () => {
       if (planId) {
@@ -29,26 +55,14 @@ function ProblemList({ problems, showTags, listName, onDelete, planId }) {
         const planSnap = await getDoc(planRef);
         if (planSnap.exists()) {
           setPlanDetails(planSnap.data());
+          console.log(planSnap.data());
         }
       }
     };
     fetchPlanDetails();
   }, [planId]);
 
-  const fetchSubmissions = async () => {
-    // Assuming you have a way to fetch submissions
-    // This is a simplified example
-    for (const handle of userHandles) {
-      const submissions = await fetchSubmissionsForHandle(handle);
-      // checkSolvedProblems(submissions);
-    }
-    console.log();
-  };
-
-  fetchSubmissions();
-
   useEffect(() => {
-    // Fetch user handles
     if (planDetails) {
       const fetchUserHandles = async () => {
         if (auth.currentUser) {
@@ -62,29 +76,41 @@ function ProblemList({ problems, showTags, listName, onDelete, planId }) {
       fetchUserHandles();
     }
   }, [planDetails]);
-  const indexOfLastProblem = currentPage * problemsPerPage;
-  const indexOfFirstProblem = indexOfLastProblem - problemsPerPage;
-  const [localProblems, setLocalProblems] = useState(problems); // Local state to manage problems
 
-  const currentProblems = problems.slice(
-    indexOfFirstProblem,
-    indexOfLastProblem
-  );
-  const handleAddProblem = async () => {
-    const problemKey = prompt('Enter the problem key:');
-    if (problemKey) {
-      const problemRef = doc(db, 'problems', problemKey);
-      const problemSnap = await getDoc(problemRef);
-
-      if (problemSnap.exists()) {
-        const newProblem = problemSnap.data();
-        setLocalProblems([...localProblems, newProblem]); // Add new problem to the local state
-        console.log('successfully added problem');
-      } else {
-        console.log('Problem not found');
-      }
-    }
+  const fetchSubmissionsForHandle = async (handle) => {
+    // Placeholder function.
+    // In a real scenario, you would fetch this data from an external API or backend.
+    return []; // Return an array of submission objects.
   };
+
+  useEffect(() => {
+    const fetchSubmissionsAndCheckSolved = async () => {
+      const allSolvedProblems = new Set();
+      for (const handle of userHandles) {
+        const submissions = await fetchSubmissionsForHandle(handle);
+        submissions.forEach((submission) => {
+          if (submission.verdict === 'OK') {
+            allSolvedProblems.add(
+              `${submission.problem.contestId}${submission.problem.index}`
+            );
+          }
+        });
+      }
+      // Update the local problems state
+      const updatedProblems = localProblems.map((problem) => ({
+        ...problem,
+        isSolved: allSolvedProblems.has(
+          problem.id ? problem.id : `${problem.contestId}${problem.index}`
+        ),
+      }));
+      setLocalProblems(updatedProblems);
+    };
+
+    if (userHandles.length > 0 && planDetails) {
+      fetchSubmissionsAndCheckSolved();
+    }
+  }, [userHandles, planDetails, localProblems]);
+
   useEffect(() => {
     setLocalProblems(problems); // Update local state when problems prop changes
   }, [problems]);
--- src/components/TrainingComponents/PlanDetail.jsx
+++ src/components/TrainingComponents/PlanDetail.jsx
@@ -1,40 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { doc, getDoc, updateDoc} from "firebase/firestore";
-import { db, auth} from "../../config/firebase";
-import ProblemList from "../ProblemList";
-import "../componentsCSS/PlanDetailCSS.css"
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { db, auth } from '../../config/firebase';
+import ProblemList from '../ProblemList';
+import '../componentsCSS/PlanDetailCSS.css';
 function PlanDetail() {
   const { planId } = useParams();
   const [plan, setPlan] = useState(null);
   const [problemDetails, setProblemDetails] = useState([]);
   const handleProblemDelete = async (problemId) => {
     try {
-      const updatedProblems = plan.problems.filter(pId => pId !== problemId);
-      await updateDoc(doc(db, "plans", planId), { problems: updatedProblems });
+      const updatedProblems = plan.problems.filter((pId) => pId !== problemId);
+      await updateDoc(doc(db, 'plans', planId), { problems: updatedProblems });
       setPlan({ ...plan, problems: updatedProblems });
-      setProblemDetails(problemDetails.filter(p => p.id !== problemId));
+      setProblemDetails(problemDetails.filter((p) => p.id !== problemId));
     } catch (error) {
-      console.error("Error deleting problem:", error);
+      console.error('Error deleting problem:', error);
     }
   };
 
-
   const handleAddProblem = async () => {
-    const problemKey = prompt("Enter the problem key:");
+    const problemKey = prompt('Enter the problem key:');
     if (problemKey) {
-      const problemRef = doc(db, "problems", problemKey);
+      const problemRef = doc(db, 'problems', problemKey);
       const problemSnap = await getDoc(problemRef);
 
       if (problemSnap.exists()) {
         const newProblem = problemSnap.data();
         const updatedProblems = [...plan.problems, problemKey]; // Add new problem key to array
-        await updateDoc(doc(db, "plans", planId), { problems: updatedProblems });
+        await updateDoc(doc(db, 'plans', planId), {
+          problems: updatedProblems,
+        });
 
         setPlan({ ...plan, problems: updatedProblems });
         setProblemDetails([...problemDetails, newProblem]); // Add new problem data to state
       } else {
-        console.log("Problem not found");
+        console.log('Problem not found');
       }
     }
   };
@@ -42,33 +43,32 @@ function PlanDetail() {
   useEffect(() => {
     const fetchPlan = async () => {
       // Fetch the plan
-      const planRef = doc(db, "plans", planId);
+      const planRef = doc(db, 'plans', planId);
       const planSnap = await getDoc(planRef);
       if (!planSnap.exists()) {
-        console.log("No such plan!");
+        console.log('No such plan!');
         return;
       }
 
       const planData = planSnap.data();
       setPlan(planData);
 
-    
       const fetchedProblems = await Promise.all(
         planData.problems.map(async (problemKey) => {
           // First, try to fetch the problem from the database
-          const problemRef = doc(db, "problems", problemKey);
+          const problemRef = doc(db, 'problems', problemKey);
           const problemSnap = await getDoc(problemRef);
 
           if (problemSnap.exists()) {
             // Problem found in the database
-          
+
             return problemSnap.data();
           } else {
-            console.log("ERROR getting the problem rom the database")
-           }
+            console.log('ERROR getting the problem rom the database');
+          }
         })
       );
-      console.log("fetchedProblems", fetchedProblems);
+      console.log('fetchedProblems', fetchedProblems);
       setProblemDetails(fetchedProblems);
     };
 
@@ -76,7 +76,7 @@ function PlanDetail() {
   }, [planId]);
 
   if (!plan) return <div>Loading...</div>;
-  
+
   const isAuthor = auth.currentUser && plan.author_id === auth.currentUser.uid;
 
   return (
@@ -93,9 +93,9 @@ function PlanDetail() {
       <ProblemList
         plan={planId}
         listName={plan.name}
-        problems={problemDetails} 
-        showTags={true} 
-        onDelete={isAuthor ? handleProblemDelete : undefined} 
+        problems={problemDetails}
+        showTags={true}
+        onDelete={isAuthor ? handleProblemDelete : undefined}
       />
     </div>
   );
