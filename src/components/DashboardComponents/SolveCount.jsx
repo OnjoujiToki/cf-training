69880cecee51f2216e8559aa597ec68a4c441cb0
OnjoujiToki <z@moon.moe>
2023-12-07 01:42:30 -0500

    update font to test deplot

--- src/components/DashboardComponents/SolveCount.jsx
+++ src/components/DashboardComponents/SolveCount.jsx
@@ -4,15 +4,18 @@ import { Button, Card, CardBody, CardTitle, CardHeader } from 'reactstrap';
 function SolveCount({ totalProblemsSolved, updateProblemCount }) {
   return (
     <Card>
-      
       <CardHeader>
-              <h3>Total Problems Solved</h3>
-            </CardHeader>
-            <CardBody>
+        <h3>Total Problems Solved</h3>
+      </CardHeader>
+      <CardBody>
         <div className="mb-3">
-          <span className="font-weight-bold">{totalProblemsSolved}</span>
+          <span className="font-weight-bold font-size:30px">
+            {totalProblemsSolved}
+          </span>
         </div>
-        <Button color="primary" onClick={updateProblemCount} className='mt-2'>Update Problem Count</Button>
+        <Button color="primary" onClick={updateProblemCount} className="mt-2">
+          Update Problem Count
+        </Button>
       </CardBody>
     </Card>
   );
